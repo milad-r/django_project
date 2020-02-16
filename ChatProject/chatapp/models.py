cef9f87dd5aa2c66e124b38f6d26da02bab93f26
milad_rastin <miladrastin77@gmail.com>
2020-02-16 16:48:03 +0330

    add Reg_google field

--- ChatProject/chatapp/models.py
+++ ChatProject/chatapp/models.py
@@ -22,7 +22,6 @@ class User(models.Model):
     username= models.CharField(max_length=10 , unique=True , primary_key=True )
     phone_number  = models.CharField(max_length=11 , default="0" , unique=True)
     profile_image = models.ImageField(upload_to='images/profileimages', null=True , blank=True)
-
+    Reg_google    = models.CharField(max_length=20,null=True)
     def __str__(self):
         return self.username
-    
